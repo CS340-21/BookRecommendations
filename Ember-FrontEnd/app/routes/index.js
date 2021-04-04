df39143b2ee83122c2cd2beb90337519a84a3897
awillif <awillif4@vols.utk.edu>
2021-04-04 14:13:04 -0400

    chore: aws lamda config

--- Ember-FrontEnd/app/routes/index.js
+++ Ember-FrontEnd/app/routes/index.js
@@ -2,20 +2,13 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default Route.extend({
-  // router: service(),
-  async getBooks() {
-    const books = await this.store.findAll('book');
-    this.set('books', books)
-    return books;
-  },
+  store: service(),
 
   async model() {
     document.title = 'Books';
-    const books = await this.getBooks();
+    const books = this.store.findAll('book');
+    this.set('books', books);
     return books;
-    // const books = this.store.findAll('book');
-    // this.set('books', books)
-    // debugger;
   },
 
 });
