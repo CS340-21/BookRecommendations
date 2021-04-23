4c10ee9024473ffbb3cb0a4ac993b4f37dac880d
awillif <awillif4@vols.utk.edu>
2021-04-23 16:32:00 -0400

    feat:search functionality

--- Ember-FrontEnd/app/controllers/index.js
+++ Ember-FrontEnd/app/controllers/index.js
@@ -1,15 +1,27 @@
 import Controller from '@ember/controller';
 
-export default class IndexController extends Controller {
-    actions = {
+export default Controller.extend({
+    userInput: "",
+     
+    actions: {
         setSelection: function(selected) {
-            this.set('selectedOption', selected)
-            console.log(this.get('selectedOption'))
+            this.set('selectedOption', selected);
+            console.log(this.selectedOption);
         },
-        submit: function(){
-           // code to save or sendAction
-           // e.g. let selectedOption = this.get('selectedOption')
-           // this.sendAction('submit', selectedOption)
+        search: async function(){
+            console.log(this.userInput);
+            let books = {};
+            if (this.selectedOption == "inauthor") {
+                books = await this.store.query('book', {
+                    inauthor: this.userInput,
+                });
+            } else {
+                books = await this.store.query('book', {
+                    subject: this.userInput,
+                });
+            }
+              books = books.map((book) => book.toJSON({ includeId: true }));
+              this.set('model', books);
         }
     }
-}
+});
